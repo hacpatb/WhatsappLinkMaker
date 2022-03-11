dd98a573fe03ba91a9101a6a02f29337882d5186
hacpatb <hacpatb@inbox.ru>
2022-03-11 23:05:05 +0500

    Add link to telegram

--- bot.js
+++ bot.js
@@ -1,27 +1,27 @@
 //docker run -d -e token="" --name="whatsapp_bot" hacpatb/whatsapp-get-dialog-link-bot
 
-const TG = require('telegram-bot-api') //https://github.com/mast/telegram-bot-api
-const parsePhoneNumber = require('libphonenumber-js')
+const TG = require('telegram-bot-api'); //https://github.com/mast/telegram-bot-api
+const parsePhoneNumber = require('libphonenumber-js');
 
-const UrlTemplate = 'https://api.whatsapp.com/send?phone=7'
+const startMessage = `Бот, который спасет вашу телефонную книгу от добавления ненужных людей.
+Для написания в WhatsApp или Telegram необходимо добавлять человека в контакты, что не всегда удобно и нужно.
+Вместо этого, можно скинуть боту номер телефона, а в ответ получите ссылку, пройдя по которой откроется диалог с нужным вам контактом`;
+const country = 'RU';
+
+const whatsappUrlTemplate = 'https://api.whatsapp.com/send?phone=';
+const telegramUrlTemplate = 'https://t.me/';
 
 if( process.env.token == undefined || process.env.token == '' ){
-    throw 'Токен не задан'
+    throw 'Токен не задан';
 }
 
-const api = new TG({
-    token: process.env.token
-})
+const api = new TG( {token: process.env.token} );
+const messageProvider = new TG.GetUpdateMessageProvider();
 
-// Define your message provider
-const mp = new TG.GetUpdateMessageProvider()
-
-api.setMessageProvider(mp)
+api.setMessageProvider(messageProvider);
 api.start()
-.then(() => {
-    console.log('API is started')
-})
-.catch(console.err)
+.then( () => { console.log('API is started') } )
+.catch(console.err);
 
 api.on('update', update => {
 
@@ -31,19 +31,19 @@ api.on('update', update => {
     if(message_text == '/start'){
         api.sendMessage({
             chat_id: chat_id,
-            text: `Бот, который спасет вашу телефонную книгу от добавления ненужных людей.\nДля написания в WhatsApp необходимо добавлять человека в контакты, что не всегда удобно и нужно.\nВместо этого, можно скинуть боту номер телефона, а в ответ получите ссылку, пройдя по которой откроется диалог с нужным вам контактом`,
+            text: startMessage,
             parse_mode: 'Markdown',
-        })
+        });
     }
     else {
-        const phoneNumber = parsePhoneNumber( message_text , 'RU')
+        const phoneNumber = parsePhoneNumber( message_text , country)
         if( phoneNumber.isValid() ){
             api.sendMessage({
                 chat_id: chat_id,
-                text: `${UrlTemplate}${phoneNumber.nationalNumber}`,
+                text: `WhatsApp: ${whatsappUrlTemplate}${phoneNumber.number}\nTelegram: ${telegramUrlTemplate}${phoneNumber.number}`,
                 disable_web_page_preview: 'true',
                 parse_mode: 'Markdown'
-            })
+            });
         }
         else {
             api.sendMessage({
@@ -51,8 +51,8 @@ api.on('update', update => {
                 text: `Что-то с номером не так ${message_text}`,
                 disable_web_page_preview: 'true',
                 parse_mode: 'Markdown'
-            })
+            });
         } 
     }
-})
+});
 
